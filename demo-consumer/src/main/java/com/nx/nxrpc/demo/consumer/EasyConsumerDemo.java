e95b3a436b60b208a697986a3d664f1fb0d918d9
nx-xn2002 <1870230468@qq.com>
2025-03-09 17:57:27 +0800

    ✨ Finished simple demo

---
+++ demo-consumer/src/main/java/com/nx/nxrpc/demo/consumer/EasyConsumerDemo.java
@@ -0,0 +1,24 @@
+package com.nx.nxrpc.demo.consumer;
+
+import com.nx.nxrpc.demo.common.model.User;
+import com.nx.nxrpc.demo.common.service.UserService;
+import com.nx.nxrpc.proxy.ServiceProxyFactory;
+
+/**
+ * easy consumer demo
+ *
+ * @author nx-xn2002
+ */
+public class EasyConsumerDemo {
+    public static void main(String[] args) {
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
+        User user = new User();
+        user.setUsername("nx");
+        User newUser = userService.getUser(user);
+        if (newUser != null) {
+            System.out.println(newUser.getUsername());
+        } else {
+            System.out.println("newUser == null");
+        }
+    }
+}
--- demo-provider/src/main/java/com/nx/nxrpc/demo/provider/EasyProviderApplication.java
+++ demo-provider/src/main/java/com/nx/nxrpc/demo/provider/EasyProviderApplication.java
@@ -1,5 +1,11 @@
 package com.nx.nxrpc.demo.provider;
 
+import com.nx.nxrpc.demo.common.service.UserService;
+import com.nx.nxrpc.demo.provider.service.UserServiceImpl;
+import com.nx.nxrpc.registry.LocalRegistry;
+import com.nx.nxrpc.server.HttpServer;
+import com.nx.nxrpc.server.VertxHttpServer;
+
 /**
  * easy provider application
  *
@@ -7,6 +13,8 @@ package com.nx.nxrpc.demo.provider;
  */
 public class EasyProviderApplication {
     public static void main(String[] args) {
-
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
+        HttpServer httpServer = new VertxHttpServer();
+        httpServer.doStart(8080);
     }
 }
